33df32702bdfe9b58a6dd8dc4619700874baf63e
endritber <endritpb@gmail.com>
2023-11-01 14:43:55 +0100

    preprocess data

--- preprocess.py
+++ preprocess.py
@@ -5,25 +5,24 @@ import tqdm
 import numpy as np
 import torch
 
-DATA_PATH = 'data/splits'
+DATA_PATH = 'data/segments'
 OUTPUT_PATH = 'data/preprocessed'
 
 def preprocess(foundation, filename, filename_out, transform=None):
   if not filename.endswith(".mp4"): return
+  if os.path.isfile(filename_out): return
   if not os.path.exists(OUTPUT_PATH): os.makedirs(OUTPUT_PATH)
   print('preprocessing', filename, filename_out)
-  if os.path.isfile(filename_out): return
   frms = []
   for frm in tqdm.tqdm(gen_frames_from_file(filename, foundation)):
     frms.append(frm)
 
-  if frms:
-    big_x = torch.from_numpy(np.array(frms).astype(np.float32))
-    print('Saving:', big_x.shape, big_x.dtype)
-    torch.save(big_x, filename_out)
+  big_x = torch.Tensor((np.concatenate([x.reshape(1, 160, 320, 3) for x in frms])))
+  print(f"Saving -> shape:{big_x.shape}")
+  torch.save({"x": big_x}, filename_out)
 
 if __name__ == '__main__':
   foundation = get_foundation()
-  for filename in os.listdir("data/splits/"):
+  for filename in os.listdir(DATA_PATH):
     if filename.endswith('.mp4'):
-      preprocess(foundation, os.path.join("data/splits/", filename), os.path.join(OUTPUT_PATH, filename.replace('.mp4', ".pt")), None)
+      preprocess(foundation, os.path.join(DATA_PATH, filename), os.path.join(OUTPUT_PATH, filename.replace('.mp4', ".pt")), None)
--- video.py
+++ video.py
@@ -7,17 +7,17 @@ import torch
 
 
 RESIZE = (320, 160)
-DURATION = 30
+DURATION = 60
 
 def get_foundation():
   from ultralytics import YOLO
   yolo_infer = YOLO('/tmp/models/yolov8n-seg.pt')
-  def foundation(frm):
-    results = yolo_infer.predict(frm, verbose=False, device='mps') # MPS to make it faster
+  def foundation(frms):
+    results = yolo_infer.predict(frms, verbose=False, device='mps') # MPS to make it faster
     try:
       masks = results[0].masks.data
     except:
-      return frm
+      return frms
     boxes = results[0].boxes.data
     clss = boxes[:, 5]
     class_indices = torch.where((clss==2) | (clss==3) | (clss==5) | (clss==7)) # car, motorcycle, truck, bus
@@ -25,7 +25,7 @@ def get_foundation():
     class_mask = torch.any(class_masks, dim=0).int() * 255
     mask = class_mask.unsqueeze(0)
     mask = mask.expand(3, *mask.shape[1:]).permute(1, 2, 0)
-    diff = cv2.subtract(frm, np.uint8(mask.cpu().numpy()))
+    diff = cv2.subtract(frms, np.uint8(mask.cpu().numpy()))
     return diff
   return foundation
 
@@ -51,7 +51,7 @@ def gen_frames_from_file(filename, foundation):
       yield frmsq
       count+=1
 
-def split_video_into_segments(video_path, labels_path, output_path):
+def split_into_segments(video_path, labels_path, output_path):
   if not os.path.exists(output_path):
     os.makedirs(output_path)
 
@@ -67,6 +67,7 @@ def split_video_into_segments(video_path, labels_path, output_path):
   segment_labels = []
   fourcc = cv2.VideoWriter_fourcc(*'mp4v')
   pbar = tqdm(total=int(cap.get(cv2.CAP_PROP_FRAME_COUNT)))
+  
   while True:
     ret, frame = cap.read()
     if not ret:
@@ -75,15 +76,16 @@ def split_video_into_segments(video_path, labels_path, output_path):
     if frame_count % frames_per_segment == 0:
       if frame_count > 0:
         with open(f'{output_path}/train_{counter}.txt', 'w') as file:
-          file.writelines(segment_labels)
+          file.writelines(segment_labels[2:]) # Don't include the first two
         segment_labels = []
         out.release()
 
       counter += 1
       out = cv2.VideoWriter(f'{output_path}/train_{counter}.mp4', fourcc, fps, (frame.shape[1], frame.shape[0]))
-   
-    out.write(frame)
-    segment_labels.append(labels[frame_count])
+    
+    out.write(frame)    
+    if frame_count < len(labels):
+      segment_labels.append(labels[frame_count])
     frame_count += 1
     pbar.update(1)
 
@@ -96,5 +98,5 @@ def split_video_into_segments(video_path, labels_path, output_path):
   print(f"Processed {counter} segments.")
 
 if __name__ == '__main__':
-  print(f'Started creating {DURATION} sec segments...')
-  split_video_into_segments('data/train.mp4', 'data/train.txt', 'data/splits')
+  print(f'Processing {DURATION}s segments...')
+  split_into_segments('data/train.mp4', 'data/train.txt', 'data/segments')
