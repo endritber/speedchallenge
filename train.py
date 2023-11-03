8941bf7e8921c29025dde48f96ca77f6f5dc59be
endritber <endritpb@gmail.com>
2023-11-03 09:38:08 +0100

    refactor

--- train.py
+++ train.py
@@ -1,116 +1,133 @@
 #!/usr/bin/env python3
-from preprocess import OUTPUT_PATH, DATA_PATH
-from tqdm import tqdm
 import os
+import sys
 import time
-import random
 import numpy as np
 import torch
 import torch.nn as nn
-from torchvision.models import efficientnet_b2, EfficientNet_B2_Weights
+import torch.nn.functional as F
+import torchvision
+
+from tqdm import tqdm
+from preprocess import OUTPUT_PATH, DATA_PATH
 from torchvision.models._api import WeightsEnum
 from torch.hub import load_state_dict_from_url
-import torch.nn.functional as F
 
+LOAD = os.getenv('LOAD') != None
+TIMESTAMP = int(time.time())
 
+# https://github.com/pytorch/vision/issues/7744
 def get_state_dict(self, *args, **kwargs):
     kwargs.pop("check_hash")
     return load_state_dict_from_url(self.url, *args, **kwargs)
 WeightsEnum.get_state_dict = get_state_dict
 
 def build_model(pretrained=True, fine_tune=True, num_classes=1):
-  if pretrained:
-    print('INFO: loading pre-trained weights')
-  
-  model = efficientnet_b2(weights=EfficientNet_B2_Weights.IMAGENET1K_V1)
+  if pretrained: print('INFO: loading pretrained model')
+  model = torchvision.models.efficientnet_b0(pretrained=pretrained); print('Running:', model._get_name())
   for params in model.parameters():
-    if fine_tune:
-      params.requires_grad = True
-    else:
-      params.requires_grad = False
-  
+    if fine_tune: params.requires_grad = True
+    else: params.requires_grad = False
   model.classifier[1] = nn.Linear(in_features=model.classifier[1].in_features, out_features=1)
-  print(model._get_name())
   return model
 
+# Add K-Fold strategy
 def run(data, kfold=False):
   if not kfold:
     n = len(data[0])
     split = int(n*0.9)
-    train_data = (data[0][0:split], data[1][0:split])
-    val_data = (data[0][split:n], data[1][split:n])
+    train_data = [torch.cat(data[0][0:split], dim=0), torch.cat(data[1][0:split], dim=0)]
+    val_data = [torch.cat(data[0][split:n], dim=0), torch.cat(data[1][split:n], dim=0)]
     train(train_data, val_data)
 
-def test_step(tx, ty):
+def test_step(batch, data):
+  losses = []
   with torch.no_grad():
     model.eval()
-    out = model(tx)
-    loss = F.mse_loss(out, ty)
-    return loss.item()
+    for samples in (t:=tqdm(batch)):
+      inputs, targets = load_batch_samples(samples, data, device)
+      outputs = model(inputs).squeeze(1)
+      loss = F.mse_loss(outputs, targets)
+      losses.append(loss.item())
+      t.set_description(f'Validating: mse_loss {loss.item():.3f}')
+    return losses
 
-def train_step(tx, ty):
-  optimizer.zero_grad()
-  out = model(tx)
-  loss = F.mse_loss(out, ty)
-  loss.backward()
-  optimizer.step()
-  return loss.item()
+def train_step(batch, data, epoch):
+  model.train()
+  losses = []
+  for samples in (t:=tqdm(batch)):
+    optimizer.zero_grad()
+    inputs, targets = load_batch_samples(samples, data, device)
+    outputs = model(inputs).squeeze(1)
+    loss = F.mse_loss(outputs, targets)
+    losses.append(loss.item())
+    loss.backward()
+    optimizer.step()
+    t.set_description(f'Training: [{epoch}|{epochs}] | mse_loss: {loss.item():.3f}')      
+  return losses
 
 def train(train_data, val_data):
-  epochs = 1600
-  batch_size = 32
-
-  tx, ty = get_minibatch(val_data, 32)
+  tlosses, vlosses = [], []
+  val_batches = load_batch_indexes(np.arange(val_data[0].shape[0]), batch_size=batch_size)
   last = time.monotonic()
-  tlosses, vlosses, i = [], [], 1
-  for epoch in (t:=tqdm(range(1, epochs+1))):
-    val_loss = test_step(tx, ty)
-    x, y = get_minibatch(train_data, batch_size)
-    train_loss = train_step(x, y)
-    tlosses.append(float(train_loss))
-    vlosses.append(float(val_loss))
-    t.set_description(f'Training -> epoch: [{epoch}|{epochs}] | train_loss: {train_loss:.3f} | val_loss: {val_loss:.3f}')
+  for epoch in range(1, epochs+1):
+    val_losses = test_step(val_batches, val_data)
+    train_losses = train_step(load_batch_indexes(np.arange(train_data[0].shape[0]), \
+                batch_size=batch_size, shuffle=True), train_data, epoch)
+    tlosses.extend(val_losses)
+    vlosses.extend(train_losses)
     epoch_time = time.monotonic() - last     
-    if i % 50 == 0:
-      print(f"Training this model takes {epoch_time * epochs // 60} min, done in {epoch_time * (epochs - epoch) // 60} min")
-    last = time.monotonic()
-    i += 1
-
-  print('Saving model...')
-  torch.save(model.state_dict(), 'demo/speed_efficientnet.pt')
+    print(f"Training (time) in {epoch_time * epochs // 60} min | Done (time) in {epoch_time * (epochs - epoch) // 60} min"); last = time.monotonic()
+    save_model(fn=f"models/{model._get_name()}_{TIMESTAMP}_{epoch}_{torch.mean(torch.tensor(val_losses)).item():.2f}.pt")
+  
   import matplotlib.pyplot as plt
   plt.plot(vlosses)
   plt.plot(tlosses)
   plt.show()
 
-def load_filename(filename, i):
-  print('loading data', filename)
+def save_model(fn):
+  torch.save(model.state_dict(), fn)
+  print(f"saved model {fn} with size {os.path.getsize(fn)}")
+
+def load_filename(filename):
   x = torch.load(f'{OUTPUT_PATH}/{filename}')["x"]
   y = torch.Tensor(np.genfromtxt(os.path.join(DATA_PATH, filename.replace('.pt', '.txt'))))
   return x, y
 
-def load_speed_data():
+def load_data():
   xs, ys = [], []
-  for i, filename in enumerate(os.listdir(OUTPUT_PATH)):
+  for filename in (t:=tqdm(os.listdir(OUTPUT_PATH))):
+    t.set_description(f'loading {filename}')
     if filename.endswith('pt'):
-      x, y = load_filename(filename, i+1)
+      x, y = load_filename(filename)
       xs.append(x); ys.append(y)
   return (xs, ys)
 
-def get_minibatch(set, bs=64, device='mps'):
-  set = [(x, y) for x, y in zip(set[0], set[1])]
-  xs, ys = [], []
-  for _ in range(bs):
-    src, val = random.choice(set)
-    sel = random.randint(0, src.shape[0]-1)
-    xs.append(src[sel:sel+1])
-    ys.append(val[sel:sel+1])
-  return torch.Tensor(np.concatenate(xs, axis=0)).permute(0, 3, 1, 2).to(device), torch.Tensor(np.array(ys)).to(device)
+def load_batch_indexes(indexes, batch_size, shuffle=False):
+  if shuffle:
+    np.random.shuffle(indexes)
+  return np.array(indexes)[:len(indexes) // batch_size*batch_size].reshape(-1, batch_size)
+
+def load_batch_samples(samples, data, device):
+  X = data[0][samples].permute(0, 3, 1, 2).to(device)
+  Y = data[1][samples].to(device)
+  return X, Y
 
 if __name__ == '__main__':
-  train_set = load_speed_data()
+  device = 'mps' if torch.backends.mps.is_available() else 'cpu' 
+ 
+  epochs = 50
+  batch_size = 32
   lr = 1e-4
-  model = build_model().to('mps')
+
+  model = build_model(pretrained=True, fine_tune=True).to(device)
+  if LOAD:
+    model_path = sys.argv[-1]
+    print('INFO: loading saved model', model_path)
+    model.load_state_dict(torch.load(model_path))
+ 
   optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-  run(train_set)
+
+  dataset = load_data()
+  run(dataset)
   
