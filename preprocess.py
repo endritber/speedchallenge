8c226e82a7181000056b3b60159d43782ee3e4cc
endritber <endritpb@gmail.com>
2023-11-03 09:21:36 +0100

    refactor

--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-from video import  get_foundation, gen_frames_from_file
+import video
 import os
-import tqdm
 import numpy as np
 import torch
+
+from tqdm import tqdm
 
 DATA_PATH = 'data/segments'
 OUTPUT_PATH = 'data/preprocessed'
@@ -14,15 +15,15 @@ def preprocess(foundation, filename, filename_out, transform=None):
   if not os.path.exists(OUTPUT_PATH): os.makedirs(OUTPUT_PATH)
   print('preprocessing', filename, filename_out)
   frms = []
-  for frm in tqdm.tqdm(gen_frames_from_file(filename, foundation)):
+  for frm in tqdm(video.gen_frames_from_file(filename, foundation)):
     frms.append(frm)
 
   big_x = torch.Tensor((np.concatenate([x.reshape(1, 160, 320, 3) for x in frms])))
-  print(f"Saving -> shape:{big_x.shape}")
+  print(f"INFO: saving {filename_out} | shape:{big_x.shape}")
   torch.save({"x": big_x}, filename_out)
 
 if __name__ == '__main__':
-  foundation = get_foundation()
+  foundation = video.get_foundation()
   for filename in os.listdir(DATA_PATH):
     if filename.endswith('.mp4'):
       preprocess(foundation, os.path.join(DATA_PATH, filename), os.path.join(OUTPUT_PATH, filename.replace('.mp4', ".pt")), None)
