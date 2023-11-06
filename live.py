0ce309df1b44e1bc05f0b7fc005a6bd34fd34cdc
endritber <endritpb@gmail.com>
2023-11-06 14:17:06 +0100

    rename

--- live.py
+++ live.py
@@ -9,15 +9,14 @@ WINDOWS = 25
 
 if __name__ == '__main__':
   device = 'mps' if torch.backends.mps.is_available() else 'cpu' 
-  model = build_model(fine_tune=False).to(device)
-  model.load_state_dict(torch.load("models/EfficientNet_1699127602_50.pt"))
+  model = build_model(fine_tune=False, weights=None).to(device)
+  model.load_state_dict(torch.load("models/EfficientNet_1699274395_4.pt"))
   model.eval()
   foundation = get_foundation()
   outs = []
   for frm, original_frame in gen_frames_from_file('data/test.mp4', foundation):  
     out = model(torch.tensor(frm, dtype=torch.float32).to(device).unsqueeze(0)).cpu().item()
-    out_to_show = running_mean(out.cpu().numpy(), WINDOWS)  
-    cv2.putText(original_frame, 'Predicted Speed: ' + str(round(out_to_show, 1)) + ' m/h', (10, 58), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (124,252,0), 2)
+    cv2.putText(original_frame, 'Predicted Speed: ' + str(round(out, 1)) + ' m/h', (10, 58), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (124,252,0), 2)
     cv2.imshow('frame', original_frame)
     if cv2.waitKey(1) == ord('q'):
       break
